6af672515269369620ef71196451f82fdfda0ff7
Inés Carrión <ines.carrion@nomasystems.com>
2024-10-08 23:52:01 +0200

    Initial Commit

---
+++ Eventualmente/EventualmenteApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventualmenteApp.swift
+//  Eventualmente
+//
+//  Created by Inés Carrión on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct EventualmenteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EventualmenteTests/EventualmenteTests.swift
@@ -0,0 +1,17 @@
+//
+//  EventualmenteTests.swift
+//  EventualmenteTests
+//
+//  Created by Inés Carrión on 8/10/24.
+//
+
+import Testing
+@testable import Eventualmente
+
+struct EventualmenteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
