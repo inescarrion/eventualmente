a7cbaf5f594d6ddea4ef3575edcfb735cae958aa
Inés Carrión <ines.carrion@nomasystems.com>
2024-11-19 21:34:38 +0100

    feat: add firebase configuration (#2)

--- Eventualmente/EventualmenteApp.swift
+++ Eventualmente/EventualmenteApp.swift
@@ -5,10 +5,25 @@
 //  Created by Inés Carrión on 8/10/24.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication
+            .LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
 
 @main
 struct EventualmenteApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
